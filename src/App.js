26529812e5c1a2fa93bedc46ed58ab7a10682a47
Luciano <91077377+Luciano-C@users.noreply.github.com>
2022-04-09 17:45:27 -0400

    Obtiene data de api y actualiza la lista de tareas

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import './App.css';
 import { useState, useEffect } from "react";
-import {ListItem} from "./Components/ListItem"
+import { ListItem } from "./Components/ListItem"
 
 function App() {
 
   const [itemList, setItemList] = useState([]);
   const [inputValue, setInputValue] = useState("");
   const [listItemsHTML, setListItemsHTML] = useState([]);
+
   
+  const getData = async () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/luckybollo")
+    .then(response => response.json())
+    .then(result => setDataFromListItem(result.map(x => x.label)))
+    .catch(error => console.log('error', error));
+  };
+
+  useEffect(() => {
+   getData();
+  }, []);
+
   // Función auxiliar para que el ListItem pueda pasar la lista actualizada a App
   const [dataFromListItem, setDataFromListItem] = useState([]);
   const passData = (data) => {
     setDataFromListItem(data);
-  }
- // Use effect que re-renderiza cuando la lista cambia.
+  };
+
+
+  // Use effect que re-renderiza cuando la lista cambia.
   useEffect(() => {
     setItemList(dataFromListItem);
-    setListItemsHTML(dataFromListItem.map((x, i) => <ListItem key={i} id={`task${i}`} value={x} list={itemList} passData={passData}/>))
-  },[dataFromListItem, itemList])
+    setListItemsHTML(dataFromListItem.map((x, i) => <ListItem key={i} id={`task${i}`} value={x} list={itemList} passData={passData} />))
+  }, [dataFromListItem, itemList])
 
 
   // Función para añadir item en el input
   const addItem = () => {
     setItemList(itemList.push(inputValue));
-    let itemsToAdd = itemList.map((x, i) => <ListItem key={i} id={`task${i}`} value={x} list={itemList} passData={passData}/>);
+    let itemsToAdd = itemList.map((x, i) => <ListItem key={i} id={`task${i}`} value={x} list={itemList} passData={passData} />);
     setListItemsHTML(itemsToAdd);
     setInputValue("");
   }
@@ -36,7 +50,7 @@ function App() {
       <div className="paperContainer">
         <input id="inputToDo" type="text" onChange={e => setInputValue(e.target.value)} onKeyPress={e => { if (e.key === "Enter") { addItem() } }} value={inputValue} placeholder="What needs to be done?" />
         <div id="paper-1">
-          <ul>{listItemsHTML.length > 0 ? listItemsHTML: "No tasks, add a task." }</ul>
+          <ul>{listItemsHTML.length > 0 ? listItemsHTML : "No tasks, add a task."}</ul>
           <p id='itemsLeft'>{itemList.length} items left</p>
         </div>
         <div id="paper-2"></div>
